66c9dbc8fd5ee14d2ca4a74c3a28c1bb7c46df73
nagafuchi1016 <gachihoko.114514@gmail.com>
2019-12-17 23:00:15 +0900

    サンプル追加

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.myapplication;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Timer mainTimer;                    //タイマー用
+    private MainTimerTask mainTimerTask;       //タイマタスククラス
+    private TextView countText;                //テキストビュー
+    private int count = 0;                      //カウント
+    private Handler mHandler = new Handler();   //UI Threadへのpost用ハンドラ
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //タイマーインスタンス生成
+        this.mainTimer = new Timer();
+        //タスククラスインスタンス生成
+        this.mainTimerTask.schedule(mainTimerTask,1000,500);
+        //テキストビュー
+        this.countText = (TextView)findViewById(R.id.count_text);
+    }
+
+    @Override
+    public boolean onCreanteOptionMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action ber if is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    //タイマータスク派生クラス
+    //run()に定周期で処理したい内容を記述
+
+    public class MainTimerTask extends TimerTask{
+        @Override
+        public void run(){
+            //ここに定周期で実行したい処理を記述
+            mHandler.post(new Runnable() {
+                public void run() {
+
+                    //実行間隔分を加算処理
+                    count += 1;
+                    //画面にカウントを表示
+                    countText.setText(String.valueOf(count));
+
+                }
+            });
+        }
+    }
+
+}
